ef5a271465e41dc9e726efff37c60e615dd414f3
tptindev <40799487+tptindev@users.noreply.github.com>
2022-07-21 19:41:40 +0700

    Delete build directory

--- build/tests/unit_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "D:/fpt/code/TinTP1_assignment_unit_test/build/tests/unit_tests[1]_tests.cmake")
-  include("D:/fpt/code/TinTP1_assignment_unit_test/build/tests/unit_tests[1]_tests.cmake")
-else()
-  add_test(unit_tests_NOT_BUILT unit_tests_NOT_BUILT)
-endif()
